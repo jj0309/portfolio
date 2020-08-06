908b05fc6c632ab0df43339c9da9ca0a0a6497da
ka-son chau <ka.alex.chau@gmail.com>
2020-08-06 03:49:36 -0400

    overhaul of some stuffs

--- public/index/js/index.js
+++ public/index/js/index.js
@@ -1,9 +1,12 @@
 let imgSlideStates={};
 window.addEventListener('load',()=>{
+    const name = 'Ka-son Chau';
     appendProjects();
     const nbOfProjects = countObjSize(ProjectsObj);
     initImgStates(nbOfProjects,imgSlideStates);
     gsap.fromTo(document.querySelector('.PresentationCard'),{opacity:0},{opacity:1,duration:2});
+    // animation of typing name in presentation card
+    animationTyping(name,document.querySelector('.PresentationCard h2'));
     setInterval(() => {
         rollImgSlides();
     }, 5000);
@@ -12,4 +15,21 @@ window.addEventListener('load',()=>{
 const initImgStates=(nbOfProjects,imgStateObj)=>{
     for(let index=0;index<nbOfProjects;index++)
         imgStateObj[index]=0;
+}
+
+/* 
+    function to animate typing of a text
+    param1:text to animate
+    param2:node in which we animate
+ */
+const animationTyping=(Text,node)=>{
+    let typed = '';
+    let index = 0;
+    let timer = setInterval(() => {
+        typed+=Text[index];
+        node.innerHTML = typed;
+        index++;
+        if(index>=Text.length)
+            clearInterval(timer);
+    }, 300);
 }
